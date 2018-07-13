d7fabe182d107b5e2f04a988fed318ec9c28b81c
Francesco Biscani <bluescarni@gmail.com>
2018-07-13 17:30:44 +0200

    tuple_for_each improvements.

--- include/rakau/tree.hpp
+++ include/rakau/tree.hpp
@@ -138,20 +138,28 @@ private:
 #endif
 };
 
-// Tuple for_each(). Execute the functor f on each element of the input Tuple.
-// https://isocpp.org/blog/2015/01/for-each-arg-eric-niebler
-// https://www.reddit.com/r/cpp/comments/2tffv3/for_each_argumentsean_parent/
-// https://www.reddit.com/r/cpp/comments/33b06v/for_each_in_tuple/
-template <typename T, typename F, std::size_t... Is>
-inline void apply_to_each_item(T &&t, const F &f, std::index_sequence<Is...>)
+// Tuple for_each(). It will apply the input functor f to each element of
+// the input tuple tup, sequentially.
+template <typename Tuple, typename F>
+inline void tuple_for_each(Tuple &&tup, F &&f)
 {
-    (void)std::initializer_list<int>{0, (void(f(std::get<Is>(std::forward<T>(t)))), 0)...};
-}
-
-template <class Tuple, class F>
-inline void tuple_for_each(Tuple &&t, const F &f)
-{
-    apply_to_each_item(std::forward<Tuple>(t), f, std::make_index_sequence<std::tuple_size_v<std::decay_t<Tuple>>>{});
+    std::apply(
+        [&f](auto &&... items) {
+            // NOTE: here we converting to void the results of the invocations
+            // of f. This ensures that we are folding using the builtin comma
+            // operator, which implies sequencing:
+            // """
+            //  Every value computation and side effect of the first (left) argument of the built-in comma operator is
+            //  sequenced before every value computation and side effect of the second (right) argument.
+            // """"
+            // NOTE: we are writing this as a right fold, i.e., it will expand as:
+            //
+            // f(tup[0]), (f(tup[1])), f(tup[2])...
+            //
+            // A left fold would also work guaranteeing the same sequencing.
+            (void(std::forward<F>(f)(std::forward<decltype(items)>(items))), ...);
+        },
+        std::forward<Tuple>(tup));
 }
 
 template <std::size_t NDim, typename Out>
