59b98696efa77e9c996d571db89ff1bae0a71d66
Francesco Biscani <bluescarni@gmail.com>
2018-08-13 12:33:35 +0200

    SIMD rotation is not needed any more.

--- include/rakau/detail/simd.hpp
+++ include/rakau/detail/simd.hpp
@@ -9,7 +9,6 @@
 #ifndef RAKAU_DETAIL_SIMD_HPP
 #define RAKAU_DETAIL_SIMD_HPP
 
-#include <algorithm>
 #include <atomic>
 #include <tuple>
 #include <type_traits>
@@ -68,113 +67,6 @@ inline auto xsimd_sqrt(B x)
 #endif
     return xsimd::sqrt(x);
 }
-
-// Rotate left by 1 the input batch x. That is, if the input
-// contains the values [0, 1, 2, 3], the output will contain
-// the values [1, 2, 3, 0].
-template <typename F, std::size_t N>
-inline xsimd::batch<F, N> rotate(xsimd::batch<F, N> x)
-{
-    // The generic (slow) implementation.
-    constexpr auto align = xsimd::simd_batch_traits<xsimd::batch<F, N>>::align;
-    alignas(align) F tmp[N];
-    x.store_aligned(tmp);
-    std::rotate(tmp, tmp + 1, tmp + N);
-    return xsimd::batch<F, N>(tmp, xsimd::aligned_mode{});
-}
-
-#if XSIMD_X86_INSTR_SET >= XSIMD_X86_AVX512_VERSION
-
-// NOTE: on AVX/SSE this looks like a *right* rotation because
-// of the way vector element are indexed in AVX/SSE intrinsics.
-
-// AVX512, float.
-template <>
-inline xsimd::batch<float, 16> rotate(xsimd::batch<float, 16> x)
-{
-    return _mm512_permutexvar_ps(_mm512_set_epi32(0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1), x);
-}
-
-// AVX512, double.
-template <>
-inline xsimd::batch<double, 8> rotate(xsimd::batch<double, 8> x)
-{
-    return _mm512_permutexvar_pd(_mm512_set_epi64(0, 7, 6, 5, 4, 3, 2, 1), x);
-}
-
-#endif
-
-#if XSIMD_X86_INSTR_SET >= XSIMD_X86_AVX2_VERSION
-
-// AVX2 for floats.
-template <>
-inline xsimd::batch<float, 8> rotate(xsimd::batch<float, 8> x)
-{
-    // This instruction is available only on AVX2, and only for floats.
-    return _mm256_permutevar8x32_ps(x, _mm256_set_epi32(0, 7, 6, 5, 4, 3, 2, 1));
-}
-
-#elif XSIMD_X86_INSTR_SET >= XSIMD_X86_AVX_VERSION
-
-// AVX1 for floats.
-template <>
-inline xsimd::batch<float, 8> rotate(xsimd::batch<float, 8> x)
-{
-    // If we have only AVX1, we use a mix of permute and blend for floats.
-    //
-    // See the second answer here:
-    // https://stackoverflow.com/questions/19516585/shifting-sse-avx-registers-32-bits-left-and-right-while-shifting-in-zeros
-    //
-    // This is slightly modified in the second line as we don't want the zeroing
-    // feature provided by _mm256_permute2f128_ps.
-    //
-    // NOTE: 0x39 == 0011 1001, 0x88 == 1000 1000.
-    // NOTE: x = [x7 x6 ... x0].
-    const auto t0 = _mm256_permute_ps(x, 0x39);        // [x4  x7  x6  x5  x0  x3  x2  x1]
-    const auto t1 = _mm256_permute2f128_ps(t0, t0, 1); // [x0  x3  x2  x1  x4  x7  x6  x5]
-    return _mm256_blend_ps(t0, t1, 0x88);              // [x0  x7  x6  x5  x4  x3  x2  x1]
-}
-
-#endif
-
-#if XSIMD_X86_INSTR_SET >= XSIMD_X86_AVX_VERSION
-
-// AVX1/2, double.
-template <>
-inline xsimd::batch<double, 4> rotate(xsimd::batch<double, 4> x)
-{
-    // NOTE: same idea as above, just different constants.
-    // NOTE: x = [x3 x2 x1 x0].
-    const auto t0 = _mm256_permute_pd(x, 5);           // [x2 x3 x0 x1]
-    const auto t1 = _mm256_permute2f128_pd(t0, t0, 1); // [x0 x1 x2 x3]
-    return _mm256_blend_pd(t0, t1, 10);                // [x0 x3 x2 x1]
-}
-
-#endif
-
-#if XSIMD_X86_INSTR_SET >= XSIMD_X86_SSE2_VERSION
-
-// SSE2, double.
-// NOTE: this requires specifically SSE2, while the float counterpart
-// requires only SSE1.
-template <>
-inline xsimd::batch<double, 2> rotate(xsimd::batch<double, 2> x)
-{
-    return _mm_shuffle_pd(x, x, 1);
-}
-
-#endif
-
-#if XSIMD_X86_INSTR_SET >= XSIMD_X86_SSE_VERSION
-
-// SSE, float.
-template <>
-inline xsimd::batch<float, 4> rotate(xsimd::batch<float, 4> x)
-{
-    return _mm_shuffle_ps(x, x, 0b00111001);
-}
-
-#endif
 
 // Newton iteration step for the computation of 1/sqrt(x) with starting point y0:
 // y1 = y0/2 * (3 - x*y0**2).
