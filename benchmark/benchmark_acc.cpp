0f35347a2b3d6bb615d0af672bcfcc9610413873
Francesco Biscani <bluescarni@gmail.com>
2018-10-27 00:24:02 +0200

    Overhaul the benchmark code to be more flexible.

--- benchmark/benchmark_acc.cpp
+++ benchmark/benchmark_acc.cpp
@@ -8,98 +8,37 @@
 
 #include <array>
 #include <initializer_list>
-#include <memory>
-#include <random>
-
-#include <boost/math/constants/constants.hpp>
+#include <iostream>
+#include <optional>
 
 #include <tbb/task_scheduler_init.h>
 
 #include <rakau/tree.hpp>
 
-static constexpr unsigned nparts = 1'000'000;
-static constexpr float bsize = 10.f;
-
-static std::mt19937 rng;
-
-template <std::size_t D, typename F>
-inline std::vector<F> get_uniform_particles(std::size_t n, F size)
-{
-    rakau::simple_timer st("uniform init");
-    std::vector<F> retval(n * (D + 1u));
-    // Mass.
-    std::uniform_real_distribution<F> mdist(F(0), F(1));
-    std::generate(retval.begin(), retval.begin() + n, [&mdist]() { return mdist(rng); });
-    // Positions.
-    const auto size_limit = size / F(2) - F(0.01);
-    std::uniform_real_distribution<F> rdist(-size_limit, size_limit);
-    std::generate(retval.begin() + n, retval.end(), [&rdist]() { return rdist(rng); });
-    return retval;
-}
-
-// See: http://www.artcompsci.org/kali/vol/plummer/ch03.html
-template <typename F>
-inline std::vector<F> get_plummer_sphere(std::size_t n, F size)
-{
-    rakau::simple_timer st("plummer init");
-    std::vector<F> retval(n * 4u);
-    // Uniform [0, 1) dist.
-    std::uniform_real_distribution<F> udist(F(0), F(1));
-    // Uniform [0.1, 1.9) dist.
-    std::uniform_real_distribution<F> mdist(F(0.1), F(1.9));
-    // Average mass of 1.
-    std::generate(retval.begin(), retval.begin() + n, [&mdist]() { return mdist(rng); });
-    for (std::size_t i = 0; i < n;) {
-        // Generate a random radius.
-        const F r = F(1) / std::sqrt(std::pow(udist(rng), F(-2) / F(3)) - F(1));
-        // Generate random u, v for sphere picking.
-        const F u = udist(rng), v = udist(rng);
-        // Longitude.
-        const F lon
-            = std::clamp(F(2) * boost::math::constants::pi<F>() * u, F(0), F(2) * boost::math::constants::pi<F>());
-        // Colatitude.
-        const F colat = std::acos(std::clamp(F(2) * v - F(1), F(-1), F(1)));
-        // Compute x, y, z.
-        const F x = r * std::cos(lon) * std::sin(colat);
-        const F y = r * std::sin(lon) * std::sin(colat);
-        const F z = r * std::cos(colat);
-        const auto size_limit = size / F(2) - F(0.01);
-        if (x >= -size_limit && x < size_limit && y >= -size_limit && y < size_limit && z >= -size_limit
-            && z < size_limit) {
-            // Assign coordinates only if we fall into the domain. Otherwise, try again.
-            *(retval.begin() + n + i) = x;
-            *(retval.begin() + 2 * n + i) = y;
-            *(retval.begin() + 3 * n + i) = z;
-            ++i;
-        }
-    }
-    return retval;
-}
-
-#include <boost/lexical_cast.hpp>
+#include "common.hpp"
 
 using namespace rakau;
+using namespace rakau_benchmark;
 
 int main(int argc, char **argv)
 {
-    if (argc < 4) {
-        throw std::runtime_error("Need at least 3 arguments, but only " + std::to_string(argc) + " was/were provided");
-    }
     std::cout.precision(20);
-    const auto idx = boost::lexical_cast<std::size_t>(argv[1]);
-    const auto max_leaf_n = boost::lexical_cast<std::size_t>(argv[2]);
-    const auto ncrit = boost::lexical_cast<std::size_t>(argv[3]);
-    auto s_init = argc >= 5 ? std::make_unique<tbb::task_scheduler_init>(boost::lexical_cast<unsigned>(argv[4]))
-                            : std::unique_ptr<tbb::task_scheduler_init>(nullptr);
 
-    // auto parts = get_uniform_particles<3>(nparts, bsize);
-    auto parts = get_plummer_sphere(nparts, bsize);
-    tree<3, float> t(bsize,
-                     {parts.begin() + nparts, parts.begin() + 2 * nparts, parts.begin() + 3 * nparts, parts.begin()},
+    const auto [nparts, idx, max_leaf_n, ncrit, nthreads, bsize, a, theta, parinit]
+        = parse_benchmark_options<float>(argc, argv);
+
+    std::optional<tbb::task_scheduler_init> t_init;
+    if (nthreads) {
+        t_init.emplace(nthreads);
+    }
+
+    auto parts = get_plummer_sphere(nparts, a, bsize, parinit);
+
+    tree<3, float> t({parts.data() + nparts, parts.data() + 2 * nparts, parts.data() + 3 * nparts, parts.data()},
                      nparts, max_leaf_n, ncrit);
     std::cout << t << '\n';
     std::array<std::vector<float>, 3> accs;
-    t.accs_u(accs, 0.75f);
+    t.accs_u(accs, theta);
     std::cout << accs[0][t.ord_ind()[idx]] << ", " << accs[1][t.ord_ind()[idx]] << ", " << accs[2][t.ord_ind()[idx]]
               << '\n';
     auto eacc = t.exact_acc_u(t.ord_ind()[idx]);
---
+++ benchmark/common.hpp
@@ -0,0 +1,195 @@
+// Copyright 2018 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the rakau library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef RAKAU_BENCHMARKS_COMMON_HPP
+#define RAKAU_BENCHMARKS_COMMON_HPP
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <boost/math/constants/constants.hpp>
+#include <boost/program_options.hpp>
+
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_for.h>
+
+#include <rakau/tree.hpp>
+
+namespace rakau_benchmark
+{
+
+inline thread_local std::mt19937 rng;
+
+// See: http://www.artcompsci.org/kali/vol/plummer/ch03.html
+template <typename F>
+inline std::vector<F> get_plummer_sphere(std::size_t n, F a, F size, bool parallel)
+{
+    rakau::simple_timer st("plummer init");
+    if (!std::isfinite(a) || a <= F(0)) {
+        throw std::invalid_argument("The Plummer 'a' parameter must be finite and positive, but it is "
+                                    + std::to_string(a) + " instead");
+    }
+    if (!std::isfinite(size) || size < F(0)) {
+        throw std::invalid_argument("The Plummer 'size' parameter must be finite and non-negative, but it is "
+                                    + std::to_string(size) + " instead");
+    }
+    std::vector<F> retval(n * 4u);
+    // Compute the limit for the coordinates based on the input box size. If size is zero, there is not limit.
+    const auto size_limit = (size > F(0)) ? (size / F(2) - size / F(100)) : std::numeric_limits<F>::infinity();
+    // Small helper to check that the x, y, z coords are within the limit computed above.
+    auto check_bounds = [size_limit](F x, F y, F z) {
+        return x >= -size_limit && x < size_limit && y >= -size_limit && y < size_limit && z >= -size_limit
+               && z < size_limit;
+    };
+    if (parallel) {
+        tbb::parallel_for(tbb::blocked_range<std::size_t>(0u, n), [&retval, a, n, check_bounds](const auto &range) {
+            // Uniform [0, 1) dist.
+            std::uniform_real_distribution<F> udist(F(0), F(1));
+            // Uniform [0.1, 1.9) dist (average mass of 1).
+            std::uniform_real_distribution<F> mdist(F(0.1), F(1.9));
+            // Init the thread-local rng with the first index in the range.
+            rng.seed(range.begin());
+            for (auto i = range.begin(); i != range.end();) {
+                retval[i] = mdist(rng);
+                // Generate a random radius.
+                // NOTE: reject r if it gets a non-finite value by chance.
+                F r;
+                do {
+                    r = a / std::sqrt(std::pow(udist(rng), F(-2) / F(3)) - F(1));
+                } while (!std::isfinite(r));
+                // Generate random u, v for sphere picking.
+                const F u = udist(rng), v = udist(rng);
+                // Longitude.
+                const F lon = std::clamp(F(2) * boost::math::constants::pi<F>() * u, F(0),
+                                         F(2) * boost::math::constants::pi<F>());
+                // Colatitude.
+                const F colat = std::acos(std::clamp(F(2) * v - F(1), F(-1), F(1)));
+                // Compute x, y, z and assign them if they are within the bounds.
+                const auto x = r * std::cos(lon) * std::sin(colat), y = r * std::sin(lon) * std::sin(colat),
+                           z = r * std::cos(colat);
+                if (check_bounds(x, y, z)) {
+                    retval[n + i] = x;
+                    retval[2u * n + i] = y;
+                    retval[3u * n + i] = z;
+                    ++i;
+                }
+            }
+        });
+        return retval;
+    } else {
+        // Uniform [0, 1) dist.
+        std::uniform_real_distribution<F> udist(F(0), F(1));
+        // Uniform [0.1, 1.9) dist.
+        std::uniform_real_distribution<F> mdist(F(0.1), F(1.9));
+        // Average mass of 1.
+        std::generate(retval.data(), retval.data() + n, [&mdist]() { return mdist(rng); });
+        for (std::size_t i = 0; i < n;) {
+            // Generate a random radius.
+            F r;
+            do {
+                r = a / std::sqrt(std::pow(udist(rng), F(-2) / F(3)) - F(1));
+            } while (!std::isfinite(r));
+            // Generate random u, v for sphere picking.
+            const F u = udist(rng), v = udist(rng);
+            // Longitude.
+            const F lon
+                = std::clamp(F(2) * boost::math::constants::pi<F>() * u, F(0), F(2) * boost::math::constants::pi<F>());
+            // Colatitude.
+            const F colat = std::acos(std::clamp(F(2) * v - F(1), F(-1), F(1)));
+            // Compute x, y, z.
+            const auto x = r * std::cos(lon) * std::sin(colat), y = r * std::sin(lon) * std::sin(colat),
+                       z = r * std::cos(colat);
+            if (check_bounds(x, y, z)) {
+                *(retval.data() + n + i) = x;
+                *(retval.data() + 2 * n + i) = y;
+                *(retval.data() + 3 * n + i) = z;
+                ++i;
+            }
+        }
+        return retval;
+    }
+}
+
+template <std::size_t D, typename F>
+inline std::vector<F> get_uniform_particles(std::size_t n, F size)
+{
+    rakau::simple_timer st("uniform init");
+    std::vector<F> retval(n * (D + 1u));
+    // Mass.
+    std::uniform_real_distribution<F> mdist(F(0), F(1));
+    std::generate(retval.begin(), retval.begin() + n, [&mdist]() { return mdist(rng); });
+    // Positions.
+    const auto size_limit = size / F(2) - F(0.01);
+    std::uniform_real_distribution<F> rdist(-size_limit, size_limit);
+    std::generate(retval.begin() + n, retval.end(), [&rdist]() { return rdist(rng); });
+    return retval;
+}
+
+template <typename F>
+inline auto parse_benchmark_options(int argc, char **argv)
+{
+    namespace po = boost::program_options;
+
+    unsigned long nparts, idx;
+    unsigned max_leaf_n, ncrit, nthreads;
+    F bsize, a, theta;
+    bool parinit = false;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()("help", "produce help message")(
+        "nparts", po::value<unsigned long>(&nparts)->default_value(1'000'000ul), "number of particles")(
+        "idx", po::value<unsigned long>(&idx)->default_value(0ul), "index of the particle to test against")(
+        "max_leaf_n", po::value<unsigned>(&max_leaf_n)->default_value(rakau::default_max_leaf_n),
+        "max number of particles in a leaf node")("ncrit",
+                                                  po::value<unsigned>(&ncrit)->default_value(rakau::default_ncrit),
+                                                  "maximum number of particles in a critical node")(
+        "a", po::value<F>(&a)->default_value(F(1)), "Plummer core radius")(
+        "bsize", po::value<F>(&bsize)->default_value(F(0)), "size of the domain (if 0, it is automatically deduced)")(
+        "nthreads", po::value<unsigned>(&nthreads)->default_value(0u),
+        "number of threads to use (0 for auto-detection)")("theta", po::value<F>(&theta)->default_value(F(0.75)),
+                                                           "theta parameter")(
+        "parinit", "parallel nondeterministic initialisation of the particle distribution");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        std::exit(0);
+    }
+
+    if (nparts == 0u) {
+        throw std::invalid_argument("The number of particles cannot be zero");
+    }
+
+    if (idx >= nparts) {
+        throw std::invalid_argument(
+            "The index of the particle to test against needs to be less-than the total number of particles ("
+            + std::to_string(nparts) + ")");
+    }
+
+    if (vm.count("parinit")) {
+        parinit = true;
+    }
+
+    return std::make_tuple(nparts, idx, max_leaf_n, ncrit, nthreads, bsize, a, theta, parinit);
+}
+
+} // namespace rakau_benchmark
+
+#endif
