47b347a5b2c21ee641df98e81ca0004190cf1ecf
Francesco Biscani <bluescarni@gmail.com>
2018-10-27 00:24:02 +0200

    Overhaul the benchmark code to be more flexible.

--- cmake/RakauFindBoost.cmake
+++ cmake/RakauFindBoost.cmake
@@ -1,5 +1,5 @@
-# Boost spreadsort is available since 1.58.
-find_package(Boost 1.58.0 REQUIRED)
+find_package(Boost 1.58.0 REQUIRED COMPONENTS ${_RAKAU_BOOST_COMPONENTS})
+
 # Might need to recreate targets if they are missing (e.g., older CMake versions).
 if(NOT TARGET Boost::boost)
     message(STATUS "The 'Boost::boost' target is missing, creating it.")
@@ -13,3 +13,20 @@ if(NOT TARGET Boost::disable_autolinking)
         set_target_properties(Boost::disable_autolinking PROPERTIES INTERFACE_COMPILE_DEFINITIONS "BOOST_ALL_NO_LIB")
     endif()
 endif()
+
+foreach(_RAKAU_BOOST_COMPONENT ${_RAKAU_BOOST_COMPONENTS})
+    if(NOT TARGET Boost::${_RAKAU_BOOST_COMPONENT})
+        message(STATUS "The 'Boost::${_RAKAU_BOOST_COMPONENT}' imported target is missing, creating it.")
+        string(TOUPPER ${_RAKAU_BOOST_COMPONENT} _RAKAU_BOOST_UPPER_COMPONENT)
+        if(Boost_USE_STATIC_LIBS)
+            add_library(Boost::${_RAKAU_BOOST_COMPONENT} STATIC IMPORTED)
+        else()
+            add_library(Boost::${_RAKAU_BOOST_COMPONENT} UNKNOWN IMPORTED)
+        endif()
+        set_target_properties(Boost::${_RAKAU_BOOST_COMPONENT} PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIRS}")
+        set_target_properties(Boost::${_RAKAU_BOOST_COMPONENT} PROPERTIES
+            IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+            IMPORTED_LOCATION "${Boost_${_RAKAU_BOOST_UPPER_COMPONENT}_LIBRARY}")
+    endif()
+endforeach()
