5fbe855c7a43da871a61bb86eef7779aaec79381
Francesco Biscani <bluescarni@gmail.com>
2018-11-07 16:03:37 +0100

    Revert "Permutation attempt."
    This reverts commit 843e4c9b560b13a12dcd2644283eefdb637bbb02.

--- include/rakau/tree.hpp
+++ include/rakau/tree.hpp
@@ -1050,14 +1050,18 @@ private:
             // Apply the permutation to the data members.
             // These steps can be done in parallel.
             simple_timer st_p("permute");
-            apply_isort(m_codes, m_isort);
-            // Make sure the sort worked as intended.
-            assert(std::is_sorted(m_codes.begin(), m_codes.end()));
+            tbb::task_group tg;
+            tg.run([this]() {
+                apply_isort(m_codes, m_isort);
+                // Make sure the sort worked as intended.
+                assert(std::is_sorted(m_codes.begin(), m_codes.end()));
+            });
             for (std::size_t j = 0; j < NDim + 1u; ++j) {
-                apply_isort(m_parts[j], m_isort);
+                tg.run([this, j]() { apply_isort(m_parts[j], m_isort); });
             }
             // Establish the indices for ordered iteration.
-            isort_to_ord_ind();
+            tg.run([this]() { isort_to_ord_ind(); });
+            tg.wait();
         }
         // Now let's proceed to the tree construction.
         // NOTE: this function works ok if N == 0.
