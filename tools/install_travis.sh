47e5e717ec1c68f591bcca58e9b46943dd17be6b
Francesco Biscani <bluescarni@gmail.com>
2018-12-04 17:26:16 +0100

    Add a build with disabled simd.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -14,4 +14,8 @@ if [[ "${RAKAU_BUILD}" == "gcc7_debug" ]]; then
     CXX=g++-7 CC=gcc-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" ../;
     make -j2 VERBOSE=1;
     ctest -V;
+elif [[ "${RAKAU_BUILD}" == "gcc7_debug_nosimd" ]]; then
+    CXX=g++-7 CC=gcc-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -DRAKAU_DISABLE_SIMD" ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
 fi
