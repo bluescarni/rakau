3972efde095c443933177f19a8d9039a904c939c
Francesco Biscani <bluescarni@gmail.com>
2018-12-04 17:27:29 +0100

    Add a build with march=native.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -18,4 +18,8 @@ elif [[ "${RAKAU_BUILD}" == "gcc7_debug_nosimd" ]]; then
     CXX=g++-7 CC=gcc-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -DRAKAU_DISABLE_SIMD" ../;
     make -j2 VERBOSE=1;
     ctest -V;
+elif [[ "${RAKAU_BUILD}" == "gcc7_debug_native" ]]; then
+    CXX=g++-7 CC=gcc-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -march=native" ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
 fi
