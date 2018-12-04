8ef0b1167bbc20c2b9a6e8ac140721fc3b4204c5
Francesco Biscani <bluescarni@gmail.com>
2018-12-04 16:47:22 +0100

    Dump of initial travis files.

---
+++ tools/install_travis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="$deps_dir/bin:$PATH"
+
+if [[ "${RAKAU_BUILD}" == "gcc7_debug" ]]; then
+    CXX=g++-7 CC=gcc-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+fi
