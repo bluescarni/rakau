d48fa16d3547b02570af41a3544f9ea8efb15aaa
Francesco Biscani <bluescarni@gmail.com>
2018-12-09 16:27:59 +0100

    First stab at osx travis.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -14,7 +14,11 @@ if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
     echo "Processor name: `cat /proc/cpuinfo|grep -i "model name"|uniq|awk -F ':' '{print $2}'`"
 fi
 
-if [[ "${RAKAU_BUILD}" == "gcc7_debug" ]]; then
+if [[ "${RAKAU_BUILD}" == "osx_debug" ]]; then
+    CXX=clang++ cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DRAKAU_TEST_NSPLIT=${TEST_NSPLIT} -DRAKAU_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+elif [[ "${RAKAU_BUILD}" == "gcc7_debug" ]]; then
     CXX=g++-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" -DRAKAU_TEST_NSPLIT=${TEST_NSPLIT} -DRAKAU_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
     make -j2 VERBOSE=1;
     ctest -V;
