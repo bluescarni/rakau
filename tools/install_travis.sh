bba588b5888da246b124de763a663e1559f7e3be
Francesco Biscani <bluescarni@gmail.com>
2019-07-16 10:48:37 +0200

    Disable the coll test on travis for some builds.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -25,13 +25,16 @@ elif [[ "${RAKAU_BUILD}" == "gcc7_debug" ]]; then
 elif [[ "${RAKAU_BUILD}" == "gcc7_debug_nosimd" ]]; then
     CXX=g++-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -DRAKAU_DISABLE_SIMD" -DRAKAU_TEST_NSPLIT=${TEST_NSPLIT} -DRAKAU_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
     make -j2 VERBOSE=1;
-    ctest -V;
+    # Don't run the collision test, as there's no SIMD-specific code in there.
+    ctest -V -E coll;
 elif [[ "${RAKAU_BUILD}" == "gcc7_debug_native" ]]; then
     CXX=g++-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -march=native" -DRAKAU_TEST_NSPLIT=${TEST_NSPLIT} -DRAKAU_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
     make -j2 VERBOSE=1;
-    ctest -V;
+    # Don't run the collision test, as there's no SIMD-specific code in there.
+    ctest -V -E coll;
 elif [[ "${RAKAU_BUILD}" == "gcc7_debug_native_norsqrt" ]]; then
     CXX=g++-7 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DRAKAU_BUILD_TESTS=yes -DRAKAU_ENABLE_RSQRT=no -DCMAKE_CXX_FLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -march=native" -DRAKAU_TEST_NSPLIT=${TEST_NSPLIT} -DRAKAU_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
     make -j2 VERBOSE=1;
-    ctest -V;
+    # Don't run the collision test, as there's no SIMD-specific code in there.
+    ctest -V -E coll;
 fi
